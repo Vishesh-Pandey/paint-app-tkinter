292dba791238112eecd78c133e12ab5c92d402ca
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2022-11-06 05:10:28 +0530

    6 - Added Tools Section

--- main.py
+++ main.py
@@ -5,7 +5,21 @@ root.title("Paint App")
 root.geometry("1100x600")
 
 frame1 = Frame(root , height=100 , width=1100 , bg="red")
-frame1.grid(row=0 , column=0)
+frame1.grid(row=0 , column=0, sticky=NW)
+
+toolsFrame = Frame(frame1 , height=100 , width=100 , bg="green")
+toolsFrame.grid(row=0 , column=0 )
+
+pencilButton = Button(toolsFrame , text="Pencil" , width=10)
+pencilButton.grid(row=0 , column=0)
+
+eraserButton = Button(toolsFrame , text="Eraser" , width=10)
+eraserButton.grid(row=1 , column=0)
+
+toolsLabel = Label(toolsFrame , text="Tools", width=10)
+toolsLabel.grid(row=3 , column=0)
+
+
 
 frame2 = Frame(root , height=500 , width=1100 , bg="yellow")
 frame2.grid(row=1 , column=0)
@@ -31,7 +45,11 @@ def paint(event):
 
     prevPoint = currentPoint
 
+    if event.type == "5" :
+        prevPoint = [0,0]
+
 canvas.bind("<B1-Motion>", paint)
+canvas.bind("<ButtonRelease-1>", paint)
 
 root.resizable(False , False)
 root.mainloop()
