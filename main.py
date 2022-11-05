48f0dd28a63413384d690a5f1302fe9691b3502b
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2022-11-06 04:09:36 +0530

    Video 2
    Created Frames

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ root = Tk()
 root.title("Paint App")
 root.geometry("1100x600")
 
+frame1 = Frame(root , height=100 , width=1100 , bg="red")
+frame1.grid(row=0 , column=0)
 
+frame2 = Frame(root , height=500 , width=1100 , bg="yellow")
+frame2.grid(row=1 , column=0)
+
+
+root.resizable(False , False)
 root.mainloop()
