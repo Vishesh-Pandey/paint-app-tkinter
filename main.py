7a36e297133d8fd772580b9f524bc78f46e18e8f
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2022-11-06 04:32:07 +0530

    4 - Created function to draw on canvas.

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ frame2.grid(row=1 , column=0)
 canvas = Canvas(frame2 , height=500 , width=1100 , bg="white")
 canvas.grid(row=0 , column=0)
 
-canvas.create_oval(100 , 100 , 120 , 120 , fill="black")
+def paint(event):
+    x = event.x
+    y = event.y
+    canvas.create_oval(x , y , x +20 , y + 20 , fill="black")
 
+canvas.bind("<B1-Motion>", paint)
 
 root.resizable(False , False)
 root.mainloop()
