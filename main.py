d9998ac7a2eb3c568c0301e9ca5cf40635128210
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2022-11-11 04:55:17 +0530

    9 - Changing stroke size

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ root = Tk()
 root.title("Paint App")
 root.geometry("1100x600")
 
+stroke_size = IntVar()
+stroke_size.set(1)
+
+stroke_color = StringVar()
+stroke_color.set("black")
+
+# Frame - 1 : Tools 
+
 frame1 = Frame(root , height=100 , width=1100 )
 frame1.grid(row=0 , column=0, sticky=NW)
+
+
+# toolsFrame 
 
 toolsFrame = Frame(frame1 , height=100 , width=100)
 toolsFrame.grid(row=0 , column=0 )
@@ -28,6 +39,7 @@ toolsLabel = Label(toolsFrame , text="Tools", width=10)
 toolsLabel.grid(row=3 , column=0)
 
 
+# sizeFrame 
 
 sizeFrame = Frame(frame1 , height=100 , width=100 )
 sizeFrame.grid(row=0 , column=1 )
@@ -35,9 +47,7 @@ sizeFrame.grid(row=0 , column=1 )
 defaultButton = Button(sizeFrame , text="Default" , width=10 , command=usePencil)
 defaultButton.grid(row=0 , column=0)
 
-stroke_size = IntVar()
-
-options = [1,2,3,4,5]
+options = [1,2,3,4,5,10]
 
 sizeList = OptionMenu(sizeFrame , stroke_size , *options)
 sizeList.grid(row=1 , column=0)
@@ -45,18 +55,12 @@ sizeList.grid(row=1 , column=0)
 sizeLabel = Label(sizeFrame , text="Size", width=10)
 sizeLabel.grid(row=2 , column=0)
 
-
-
-
-
+# Frame - 2 - Canvas
 frame2 = Frame(root , height=500 , width=1100 , bg="yellow")
 frame2.grid(row=1 , column=0)
 
 canvas = Canvas(frame2 , height=500 , width=1100 , bg="white" )
 canvas.grid(row=0 , column=0)
-
-stroke_color = StringVar()
-stroke_color.set("green")
 
 # variables for pencil 
 
@@ -72,7 +76,7 @@ def paint(event):
     # canvas.create_oval(x , y , x +5 , y + 5 , fill="black")
 
     if prevPoint != [0,0] : 
-        canvas.create_line(prevPoint[0] , prevPoint[1] , currentPoint[0] , currentPoint[1],fill=stroke_color.get())
+        canvas.create_polygon(prevPoint[0] , prevPoint[1] , currentPoint[0] , currentPoint[1],fill=stroke_color.get() , outline=stroke_color.get() , width=stroke_size.get())
 
     prevPoint = currentPoint
 
