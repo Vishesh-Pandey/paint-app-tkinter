e7a9bfb9819c49ee2dbedb9dd62981f334903598
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2022-11-11 04:03:38 +0530

    8 - Added Dropdown list

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ root = Tk()
 root.title("Paint App")
 root.geometry("1100x600")
 
-frame1 = Frame(root , height=100 , width=1100 , bg="red")
+frame1 = Frame(root , height=100 , width=1100 )
 frame1.grid(row=0 , column=0, sticky=NW)
 
-toolsFrame = Frame(frame1 , height=100 , width=100 , bg="green")
+toolsFrame = Frame(frame1 , height=100 , width=100)
 toolsFrame.grid(row=0 , column=0 )
 
 def usePencil():
     stroke_color.set("black")
-    
+    canvas["cursor"] = "arrow"
 
 def useEraser():
     stroke_color.set("white")
@@ -26,6 +26,26 @@ eraserButton.grid(row=1 , column=0)
 
 toolsLabel = Label(toolsFrame , text="Tools", width=10)
 toolsLabel.grid(row=3 , column=0)
+
+
+
+sizeFrame = Frame(frame1 , height=100 , width=100 )
+sizeFrame.grid(row=0 , column=1 )
+
+defaultButton = Button(sizeFrame , text="Default" , width=10 , command=usePencil)
+defaultButton.grid(row=0 , column=0)
+
+stroke_size = IntVar()
+
+options = [1,2,3,4,5]
+
+sizeList = OptionMenu(sizeFrame , stroke_size , *options)
+sizeList.grid(row=1 , column=0)
+
+sizeLabel = Label(sizeFrame , text="Size", width=10)
+sizeLabel.grid(row=2 , column=0)
+
+
 
 
 
